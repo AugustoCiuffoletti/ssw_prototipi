fc169040c0347a87f47274567a05896fa49120a9
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-23 16:04:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-sfdxy7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sfdxy7)
---
+++ index.js
@@ -0,0 +1,10 @@
+function mioPrototipo(x,y) {
+    this.a=x;
+    this.b=y;
+};
+let X = new mioPrototipo("", 0);
+let Y = new mioPrototipo;
+Y.a = "Hello";
+Y.b=2;
+console.log(X);
+console.log(Y);
