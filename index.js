fc2c934502e35379c57f221df269dbfd3e819252
AugustoCiuffoletti <augusto.ciuffoletti@unipi.it>
2023-04-04 10:05:31 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// Questo codice funziona, ma è fragile e contiene "errori"
 function mioPrototipo(x, y) {
   this.a = x;
   this.b = y;
-  this.op = x => r;
+  this.op = (x) => r;
 }
-let X = new mioPrototipo("", 0, x => x + 2);
+let X = new mioPrototipo('', 0, (x) => x + 2);
 let Y = new mioPrototipo();
-Y.a = "Hello";
-Y.b = 2; // Provate a sostituire 2 con "Hallo"
-Y.op = a => a + 3;
+Y.a = 'Hello';
+Y.b = 0; // Provate a sostituire 2 con "Hallo"
+Y.op = (a) => a + 3;
 console.log(X);
 console.log(Y);
 console.log(Y.op(6));
